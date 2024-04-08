dd342c2ba40a9a0fe2a125ab9967375e9f3842dd
Maggie McDowell <maggiemcdowell4@gmail.com>
2024-04-07 21:49:47 -0600

    this is really hard

---
+++ Assets/js/blog.js
@@ -0,0 +1,61 @@
+let blogData = JSON.parse(localStorage.getItem("blogData"))
+console.log(blogData)
+
+// for (let i = 0; i < blogData.length; i++) {
+//    console.log(`${i + 1}) ${blogData[i]}`);
+// }
+
+
+
+
+
+
+
+// //This is for the first blog entry
+// const userData = JSON.parse(localStorage.getItem("userData"))
+// console.log(userData);
+// const blogTitle = document.querySelector(".firstTitle")
+// blogTitle.textContent = userData.title
+// const blogContent = document.querySelector(".firstContent")
+// blogContent.textContent = userData.content
+// const blogName = document.querySelector(".firstName")
+// blogName.textContent = "Posted by: " + userData.userName
+
+
+// //This is for the second blog entry
+// const userData2 = JSON.parse(localStorage.getItem("userData2"))
+// console.log(userData2); if (userData2 !== null) {
+// const blogTitle2 = document.querySelector(".secondTitle")
+// blogTitle2.textContent = userData2.title
+// const blogContent2 = document.querySelector(".secondContent")
+// blogContent2.textContent = userData2.content
+// const blogName2 = document.querySelector(".secondName")
+// blogName2.textContent = "Posted by: " + userData2.userName
+// }
+
+// //This is for the third blog entry
+// const userData3 = JSON.parse(localStorage.getItem("userData3"))
+// console.log(userData3); if (userData3 !== null) {
+// const blogTitle3 = document.querySelector(".thirdTitle")
+// blogTitle3.textContent = userData3.title
+// const blogContent3 = document.querySelector(".thirdContent")
+// blogContent3.textContent = userData3.content
+// const blogName3 = document.querySelector(".thirdName")
+// blogName3.textContent = "Posted by: " + userData3.userName
+// }
+
+// //This is for the fouth blog entry
+// const userData4 = JSON.parse(localStorage.getItem("userData4"))
+// console.log(userData4); if (userData4 !== null) {
+// const blogTitle4 = document.querySelector(".fourthTitle")
+// blogTitle4.textContent = userData4.title
+// const blogContent4 = document.querySelector(".fourthContent")
+// blogContent4.textContent = userData4.content
+// const blogName4 = document.querySelector(".fourthName")
+// blogName4.textContent = "Posted by: " + userData4.userName
+// }
+
+// const backbutton = document.querySelector("#back-button");
+// backbutton.addEventListener("click", function(){
+//    window.location.href = "./index.html"
+// })
--- Assets/js/form.js
+++ Assets/js/form.js
@@ -1,30 +1,106 @@
-const name = document.getElementById("name")
+const nameEl = document.getElementById("name")
 const title = document.getElementById("title")
 const content = document.getElementById("content")
+const form = document.querySelector("form")
 
-
-const buttonEl = document.querySelector('#submitbtn');
-  // Get the button element
-buttonEl.addEventListener("click", function(event){
+form.addEventListener("submit", function(event){
   event.preventDefault()
-  const nameValue = name.value
-  console.log(nameValue)
-  window.location.href = "./blog.html"
-})
-const titleEL = document.querySelector("submitbtn");
-
-titleEL.addEventListener("click", function(event){
-  event.preventDefault()
+  const nameValue = nameEl.value
   const titleValue = title.value
-  window.location.href = "./blog.html"
-})
-const contentEl = document.querySelector("#submitbtn")
-
-contentEl.addEventListener("click", function(event){
-  event.preventDefault()
   const contentValue = content.value
-  window.location.href = "./blog.html"
-})
+  console.log(nameValue)
+  
+  let blogData = JSON.parse(localStorage.getItem("blogData"))
+  if (blogData === null) {
+    blogData = []
+    blogData.push({
+      userName: nameValue,
+      title: titleValue,
+      content: contentValue
+    })
+
+    localStorage.setItem("blogData", JSON.stringify(blogData))
+    window.location.href = "./blog.html"
+  }
+  
+  if (blogData) {
+    blogData.push({
+      userName: nameValue,
+      title: titleValue,
+      content: contentValue
+    })
+
+    localStorage.setItem("blogData", JSON.stringify(blogData))
+    window.location.href = "./blog.html"
+  }
+
+
+
+
+}
+)
+
+
+
+
+
+  // } else {
+ 
+ 
+//   const existingUser2 = JSON.parse(localStorage.getItem("userData2"))
+//   if (existingUser2 === null) {
+//   const userData2 = {userName: nameValue, title: titleValue, content: contentValue}
+//   console.log(userData2)
+
+//   localStorage.setItem("userData2", JSON.stringify(userData2))
+//   window.location.href = "./blog.html"
+//   } else {
+//   const existingUser3 = JSON.parse(localStorage.getItem("userData3"))
+//   if (existingUser3 === null) {
+//     const userData3 = {userName: nameValue, title: titleValue, content: contentValue}
+//     console.log(userData3)
+
+//   localStorage.setItem("userData3", JSON.stringify(userData3))
+//   window.location.href = "./blog.html"
+//   } else {
+//     const existingUser4 = JSON.parse(localStorage.getItem("userData4"))
+//     if (existingUser4 === null) {
+//       const userData4 = {userName: nameValue, title: titleValue, content: contentValue}
+//       console.log(userData4)
+  
+//     localStorage.setItem("userData4", JSON.stringify(userData4))
+//     window.location.href = "./blog.html"
+//   }
+// }}}
+// })
+
+
+
+
+
+
+// const buttonEl = document.querySelector('#submitbtn');
+//   // Get the button element
+// buttonEl.addEventListener("click", function(event){
+//   event.preventDefault()
+//   const nameValue = name.value
+//   console.log(nameValue)
+//   window.location.href = "./blog.html"
+// })
+// const titleEL = document.querySelector("submitbtn");
+
+// titleEL.addEventListener("click", function(event){
+//   event.preventDefault()
+//   const titleValue = title.value
+//   window.location.href = "./blog.html"
+// })
+// const contentEl = document.querySelector("#submitbtn")
+
+// contentEl.addEventListener("click", function(event){
+//   event.preventDefault()
+//   const contentValue = content.value
+//   window.location.href = "./blog.html"
+// })
 
 
 
